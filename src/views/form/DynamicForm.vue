405cc096f787c843241fc3ab83041befb7337e43
rende.zhao <351157970@qq.com>
2018-10-25 06:50:12 +0800

    功能：动态表单初步实现

--- src/views/form/DynamicForm.vue
+++ src/views/form/DynamicForm.vue
@@ -1,6 +1,121 @@
 <template>
   <div>
-    <schema-form></schema-form>
+    <component v-bind:is="tsxDemo"></component>
+    <a-form @submit="handleSubmit" hideRequiredMark style="margin-top: 8px" :autoFormCreate="(form)=>{this.form = form}">
+          <!--  <a-form-item 
+              v-bind="formItemLayout" 
+              label="标题"
+              fieldDecoratorId="title"
+              :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入标题' }]}">
+              <a-input placeholder="给目标起个名字" />
+            </a-form-item>
+
+            <a-form-item 
+              v-bind="formItemLayout"  
+              label="起止日期"
+              fieldDecoratorId="date"
+              :fieldDecoratorOptions="{rules: [{ required: true, message: '请选择起止日期' }]}">
+              <a-range-picker style="width: '100%'" :placeholder="['开始日期', '结束日期']" />
+            </a-form-item>
+
+            <a-form-item v-bind="formItemLayout" 
+              label="目标描述"
+              fieldDecoratorId="goal"
+              :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入目标描述' }]}">
+              <a-textarea
+                  style="min-height: 32px"
+                  placeholder="请输入你的阶段性工作目标"
+                  :rows="4"
+                />
+            </a-form-item>
+
+            <a-form-item  v-bind="formItemLayout"  
+              label="衡量标准"
+              fieldDecoratorId="standard"
+              :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入衡量标准' }]}">
+              <a-textarea style="min-height: 32px" placeholder="请输入衡量标准" :rows="4" />
+            </a-form-item>
+            
+            <a-form-item
+              v-bind="formItemLayout"
+              fieldDecoratorId="client"
+            >
+              <span 
+               slot="label">
+                  客户
+                  <em class="ptional">
+                    （选填）
+                    <a-tooltip title="目标的服务对象">
+                      <a-icon type="info-circle-o" :style="{ marginRight: 4 }" />
+                    </a-tooltip>
+                  </em>
+                </span>
+              <a-input placeholder="请描述你服务的客户，内部客户直接 @姓名／工号" />
+            </a-form-item>
+
+            <a-form-item
+              v-bind="formItemLayout"
+              fieldDecoratorId="invites"
+            >
+              <span slot="label">
+                  邀评人
+                  <em class="optional">（选填）</em>
+                </span>
+              <a-input placeholder="请直接 @姓名／工号，最多可邀请 5 人" />
+            </a-form-item>
+            
+            <a-form-item
+              v-bind="formItemLayout"
+              fieldDecoratorId="weight"
+            >
+            <span slot="label">
+                  权重
+                  <em class="optional">（选填）</em>
+                </span>
+              <a-input-number placeholder="请输入" :min="0" :max="100" />
+              <span class="ant-form-text">%</span>
+            </a-form-item>
+           
+            
+            <a-form-item v-bind="formItemLayout"
+              fieldDecoratorId="public" label="目标公开" help="客户、邀评人默认被分享">
+              <div>
+                <a-radio-group defaultValue="1">
+                    <a-radio value="1">公开</a-radio>
+                    <a-radio value="2">部分公开</a-radio>
+                    <a-radio value="3">不公开</a-radio>
+                  </a-radio-group>
+                  
+               <a-form-item :style="{ marginBottom: 0 }"  fieldDecoratorId="publicUsers">
+                  <a-select
+                      mode="multiple"
+                      placeholder="公开给"
+                      :style="{margin: '8px 0',
+                        display: form && form.getFieldValue('public') === '2' ? 'block' : 'none'
+                      }"
+                    >
+                      <a-select-option value="1">同事甲</a-select-option>
+                      <a-select-option value="2">同事乙</a-select-option>
+                      <a-select-option value="3">同事丙</a-select-option>
+                    </a-select>
+                </a-form-item> 
+               
+              </div>
+            </a-form-item> 
+
+              -->
+
+            
+            <component v-bind:is="formItems"></component>
+            <a-form-item style="margin-top: 32px">
+              <a-button type="primary" htmlType="submit" >
+                提交
+              </a-button>
+              <a-button style="margin-left: 8px">保存</a-button>
+            </a-form-item>
+            
+          </a-form>
+         
   </div>
 </template>
 
@@ -8,15 +123,95 @@
 
 </style>
 
-<script  lang="ts">
+<script  lang="tsx">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { State, Mutation, namespace } from 'vuex-class';
+
 
 @Component({
     components:{
     }
 })
 export default class DynamicForm extends Vue {
-  
+
+  private form:any = null;
+
+  private schema: any= {
+    propiters:[
+      {
+        type: 'string',
+        label:'姓名',
+      },
+      {
+        type: 'number',
+        label:'年龄',
+      },
+    ],
+  }
+
+  private title: string= 'hello tsx';
+
+  get tsxDemo() {
+      return {
+        render: () => <div>{this.title}</div>
+      };
+    }
+
+  private divDemo(){
+    
+    return <div>dddd</div>
+  }
+
+  get formItems(){
+    const items = <a-form-item label="标题">
+              <a-input placeholder="给目标起个名字" />
+            </a-form-item>;
+    const item =([
+      this.buildFormitem({type:'string',label:'测试'}),this.buildFormitem({type:'string',label:'测试2'})
+    ]);
+    const formitems = this.items(this.schema.propiters);
+    return {
+      render: ()=> <div>{formitems}</div>
+    };
+  }
+
+  private createformitem(){
+    const items = <a-form-item label="标题">
+              <a-input placeholder="给目标起个名字" />
+            </a-form-item>;
+    return items;
+  }
+
+  private items(formItems: any): any{
+    let formitems = [];
+    formItems.forEach((item: any)=>{
+      formitems.push(this.buildFormitem(item));
+    });
+    return formitems
+  }
+
+  private buildFormitem(formitem: any){
+    if(formitem.type === 'string'){
+      return <a-form-item
+              label={formitem.label}>
+              <a-input />
+            </a-form-item>;
+    }
+
+    if(formitem.type === 'number'){
+       return <a-form-item
+              label={formitem.label}>
+              <a-input-number />
+            </a-form-item>
+    }
+    return '';
+  }
+
+  private handleSubmit(){
+
+  }
+
+
+
 }
 </script>
---
+++ src/views/form/divdemo.vue
@@ -0,0 +1,21 @@
+<template>
+<div>ssdf</div>
+</template>
+
+<style lang="less">
+
+</style>
+
+<script  lang="tsx">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { State, Mutation, namespace } from 'vuex-class';
+
+@Component({
+    components:{
+    }
+})
+export default class Dvidemo extends Vue {
+
+
+}
+</script>
