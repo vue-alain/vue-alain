c1de8231408a552a63f908a0d38cb26307035b61
rende.zhao <351157970@qq.com>
2018-12-14 10:39:02 +0800

    refactor: reactor app state use IAppState as app store type

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -1,3 +1,57 @@
+import { Module, MutationTree, ActionTree, GetterTree } from 'vuex';
+import { RootState } from './../store';
+
+export interface Doctitle {
+  title?: string;
+  i18n?: string;
+}
+
+export interface IAppState {
+  isCollapse: boolean ;
+  name?: string;
+  doctitle?: Doctitle;
+}
+
+
+const mutations: MutationTree<IAppState> = {
+  collapse(state: IAppState) {
+    state.isCollapse = !state.isCollapse;
+  },
+  appName(state: IAppState, appinfo: any) {
+    state.name = appinfo.name;
+  },
+  changeTitle(state: IAppState, titleInfo: any) {
+    state.doctitle = {
+      ...titleInfo,
+    };
+  },
+};
+
+
+const actions: ActionTree<IAppState, RootState> = {
+
+};
+
+const getters: GetterTree<IAppState, RootState> = {
+
+};
+
+const appState: IAppState = {
+  isCollapse: false,
+  name: '',
+  doctitle: {
+    title: '',
+  },
+};
+
+const app: Module<IAppState, RootState> = {
+  namespaced: true,
+  state: appState,
+  getters,
+  actions,
+  mutations,
+};
+/*
 const app = {
     namespaced: true,
     state: {
@@ -24,6 +78,6 @@ const app = {
     actions: {
     },
   };
-
+*/
 
 export default app;
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -32,20 +32,4 @@ const user: Module<ILoginInfoState, RootState> = {
   mutations,
 };
 
-/*
-const user = {
-    namespaced: true,
-    state: {
-      token: null,
-    },
-    mutations: {
-      loginSuccess(state: any, param: any) {
-        state.token = param.token;
-      },
-    },
-    actions: {
-    },
-  };
-*/
-
 export default user;
